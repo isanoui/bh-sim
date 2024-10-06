696731dbb1b59f2c60d4ca5cbd3af336c05127bd
isanoui <isanoui13@gmail.com>
2024-10-06 14:30:38 -0700

    types and storage

---
+++ src/detectionStorage.ts
@@ -0,0 +1,3 @@
+import { DetectionMessage } from "./types/detections";
+
+export const detections: DetectionMessage[] = [];
---
+++ src/types/detections.ts
@@ -0,0 +1,15 @@
+export type Detection = {
+  drone_id: string;
+  start_time: number;
+  band: "2.4GHz" | "5.8GHz";
+  classification: "OcuSync" | "Lightbridge";
+  rssi: number;
+};
+
+export type DetectionMessage = {
+  msg_type: "detections";
+  system: "detection_service";
+  msg_id: string;
+  detections: Detection[];
+  timestamp: string;
+};
